037f4c74c7ac0bf86f543855ee914dacd0796a67
orimintz21 <ori.mintz@technion.campus.co.il>
2024-06-26 21:30:44 +0300

    Mintz: start: added the init file for the main directory for validation
    Signed-off-by: orimintz21 <ori.mintz@technion.campus.co.il>

---
+++ strategic_ml/__init__.py
@@ -0,0 +1,16 @@
+"""
+:file: __init__.py
+This is the init file for the strategic_ml package.
+"""
+# Package metadata
+__version__ = '0.1.0'
+__author__ = 'Ori Mintz and Kfir Cohen'
+
+
+# Main modules and functions
+# TODO: Add main modules and functions here
+
+# Functionality of the package
+# TODO: Add functionality here
+__all__: list = [] # List of modules and functions to be imported when using 'from strategic_ml import *'
+
