a702cd7d48444adcdccaa19ad693f761f595018d
orimintz21 <ori.mintz@technion.campus.co.il>
2024-07-12 17:49:39 +0300

    add gsc
    Signed-off-by: orimintz21 <ori.mintz@technion.campus.co.il>

---
+++ strategic_ml/models/strategic_model.py
@@ -0,0 +1,15 @@
+"""
+:file
+This file contains the basic structure for strategic model.
+When creating a new model, you must inherit from this model to use the features
+in the library.
+
+This is an interface, meaning that you are not intended to construct this class
+without implementing the relevant functions.
+
+TODO: complete the file prolog
+"""
+
+
+class StrategicModel:
+    pass
