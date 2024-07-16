278ad6041f604bf829b039b74700773c88d543ad
ori.mintz <ori.mintz@campus.technion.ac.il>
2024-07-16 13:33:15 +0300

    continue GSC, needs Model
    Signed-off-by: ori.mintz <ori.mintz@campus.technion.ac.il>

--- strategic_ml/gsc/generalized_strategic_delta.py
+++ strategic_ml/gsc/generalized_strategic_delta.py
@@ -1,26 +1,28 @@
 """:file: generalized_strategic_delta.py
+This is the interface for all of GSC models.
+When creating a new model in this framework you will need to inherit from
+it.
+A GSC - stands for generalized strategic is based on the paper:
+'Generalized Strategic Classification and the Case of Aligned Incentives'
+
+The general idea of the GSC is that a GSC gets a model, and change the input
+x to x' based on the GSC's type.
 """
 
 # Imports
 import torch
 from torch import nn
+from typing import Optional
+from strategic_ml.models import _Model
+
 
 
 # Implementation
-class GSC(nn.Module):
-    """
-    This is the interface for all of GSC models.
-    When creating a new model in this framework you will need to inherit from
-    it.
-    A GSC - stands for generalized strategic is based on the paper:
-    'Generalized Strategic Classification and the Case of Aligned Incentives'
+class _GSC(nn.Module):
+    def __init__(self, model: _Model) -> None:
+        super(_GSC, self).__init__()
+        self.model : _Model= model
 
-    The general idea of the GSC is that a GSC gets a model, and change the input
-    x to x' based on the GSC's type.
-    """
-
-    def __init__(self) -> None:
-        super(GSC, self).__init__()
+    def forward(self, x: torch.Tensor, y: torch.Tensor, y_tilde: Optional[torch.Tensor] = None) -> torch.Tensor:
 
 
-norm = torch.norm(3, "fro", 3)
