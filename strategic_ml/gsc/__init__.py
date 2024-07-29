d46a32cc230db2e8ae55118a5844fe9f0b3c85ac
ori.mintz <ori.mintz@campus.technion.ac.il>
2024-07-29 17:34:21 +0300

    add init
    Signed-off-by: ori.mintz <ori.mintz@campus.technion.ac.il>

--- strategic_ml/gsc/__init__.py
+++ strategic_ml/gsc/__init__.py
@@ -5,5 +5,7 @@ This is the init file for the GSC.
 
 from generalized_strategic_delta import _GSC
 from strategic_ml.gsc.linear_gp import _LinearGP
+from strategic_ml.gsc.linear_adv_delta import LinearAdvDelta
+from strategic_ml.gsc.linear_strategic_delta import LinearStrategicDelta
 
-__all__ = ["_GSC", "_LinearGP"]
+__all__ = ["_GSC", "_LinearGP", "LinearAdvDelta", "LinearStrategicDelta"]
