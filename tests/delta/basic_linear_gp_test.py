05dcb7a060451a1847d18dde08d1eeebb84dd48e
ori.mintz <ori.mintz@campus.technion.ac.il>
2024-08-07 12:45:08 +0300

    move create to the start of the test
    Signed-off-by: ori.mintz <ori.mintz@campus.technion.ac.il>

--- tests/delta/basic_linear_gp_test.py
+++ tests/delta/basic_linear_gp_test.py
@@ -6,10 +6,11 @@ from strategic_ml import (
     LinearStrategicModel,
     LinearStrategicDelta,
     LinearAdvDelta,
+    LinearNoisyLabelDelta
 )
 
 
-def create_data():
+def create_strategic_separable_data():
     # Set the random seed for reproducibility
     torch.manual_seed(0)
 
@@ -28,16 +29,37 @@ def create_data():
     y = torch.cat((y1, y2), dim=0)
     return x, y
 
+def create_strategic_noisy_data():
+    # Set the random seed for reproducibility
+    torch.manual_seed(0)
+
+    # Generate the first half of the points with the first index less than -5
+    x1 = torch.cat((torch.randn(5, 1) - 10, torch.randn(5, 1)), dim=1)
+
+    # Generate the second half of the points with the first index greater than 5
+    x2 = torch.cat((torch.randn(5, 1) + 10, torch.randn(5, 1)), dim=1)
+
+    # Concatenate both parts to create the dataset
+    x = torch.cat((x1, x2), dim=0)
+
+    # Create labels: 1 for the first half, -1 for the second half
+    y1 = torch.ones(5, 1)
+    y2 = -torch.ones(5, 1)
+    y = torch.cat((y1, y2), dim=0)
+
+    # Create noisy labels
+    bernoulli_tensor = torch.bernoulli(0.5 * torch.ones(y.shape))
+    y_noisy = y * bernoulli_tensor
+    return x, y_noisy
+
 
 class TestLinearStrategicDelta(unittest.TestCase):
-    def setUp(self) -> None:
-        print("Setting up the test")
-        self.x, self.y = create_data()
 
     def test_demo(self) -> None:
         """This test just checks that the flow of the code is correct.
         It does not check the correctness of the code.
         """
+        self.x, self.y = create_strategic_separable_data()
         # Create a strategic model
         strategic_model = LinearStrategicModel(in_features=2)
 
